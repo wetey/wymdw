9c030bf27241f785e02a5c479c66dae7283ae626
wetey <joodotey@gmail.com>
2024-06-25 11:39:45 -0400

    add summary

--- pages/Summary.py
+++ pages/Summary.py
@@ -4,6 +4,9 @@ st.set_page_config(page_title="Summary")
 
 st.markdown("""
     
+    We clustered the errors so the semantically similar examples are grouped together. We looked at the error type distribution per cluster. Lastly, we looked at the top words per error type. 
+            
+    With these visualization we were able to gain a better understanding of what the model is misinterpreting. 
 
 
 """)
